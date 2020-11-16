cd05e48556496718d5f3f8ac08483e49f49e83ac
LottieC99 <72411768+LottieC99@users.noreply.github.com>
2020-11-16 19:16:49 +0000

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Lottie Cottrell and is{" "}
+      <a href="https://github.com/LottieC99/react-app-project" target="_blank"> open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
