8650e1ae566e7c1a43d942751eb01b523b4af3e4
LottieC99 <72411768+LottieC99@users.noreply.github.com>
2020-11-16 19:40:49 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
-      <footer>
+        <footer>
         This project was coded by Lottie Cottrell and is{" "}
-      <a href="https://github.com/LottieC99/react-app-project" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
+         <a href="https://github.com/LottieC99/react-app-project" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
