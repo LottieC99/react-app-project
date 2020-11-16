15ea7bd2bbfb504683d777d6666f098a309603f5
LottieC99 <72411768+LottieC99@users.noreply.github.com>
2020-11-16 19:23:09 +0000

    added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by Lottie Cottrell and is{" "}
-      <a href="https://github.com/LottieC99/react-app-project" target="_blank"> open-sourced on GitHub</a>
+      <a href="https://github.com/LottieC99/react-app-project" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
       </footer>
     </div>
   );
